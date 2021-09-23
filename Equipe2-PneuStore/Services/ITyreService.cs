292b019fba8535798b146b5a7e50e7fbc6303102
Josué Barros <frajolla27@gmail.com>
2021-09-23 00:53:02 -0300

    <feat>[Interface and Service]:
    <service skeleton and interface>

---
+++ Equipe2-PneuStore/Services/ITyreService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Equipe2_PneuStore.Services
+{
+    public interface ITyreService
+    {
+        List<Tyre> All();
+
+        Tyre Get(int id);
+
+        bool Create(Tyre tyre);
+
+        bool Update(Tyre tyre);
+    }
+}
---
+++ Equipe2-PneuStore/Services/TyreService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Equipe2_PneuStore.Services
+{
+    public class TyreService : ITyreService
+    {
+        //construtor
+        public TyreService()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Tyre> All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Create(Tyre tyre)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public Tyre Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Tyre tyre)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
