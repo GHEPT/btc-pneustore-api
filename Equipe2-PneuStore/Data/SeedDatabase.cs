3dd9b4a5116befdf74edf0f8059f3699c8d7ebf6
Eduardo Meirelles Nogueira <83734364+edumeirelles@users.noreply.github.com>
2021-09-23 14:10:48 -0300

    <feat>:[Implements SeedDatabase]

--- Equipe2-PneuStore/Data/SeedDatabase.cs
+++ Equipe2-PneuStore/Data/SeedDatabase.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using ApiPneuStore.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Equipe2_PneuStore.Data
 {
     public class SeedDatabase
     {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new Context(
+                serviceProvider.GetRequiredService<DbContextOptions<Context>>()))
+            {
+                if (context.Tyre.Any())
+                {
+                    return;
+                }
 
+                for(var i=1; i <= 10; i++)
+                {
+                    context.Tyre.Add(new Tyre
+                    {
+                        Brand = $"Marca {i}",
+                        Model = $"Modelo {i}",
+                        Price = 99.99,
+                        Description = $"Descrição {i}",                       
+                    });
+                    context.Partner.Add(new Partner
+                    {
+                        Name = $"Parceiro {i}",
+                        PhoneNumber = $"({i}{i}) {i}{i}{i}{i} - {i}{i}{i}{i}",
+                        Address = $"Endereço {i}"
+                    });
+                }        
+                
+                context.SaveChanges();
+            }
+        }
     }
 }
