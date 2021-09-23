30546514b5691595acde874750791c1260eac2f3
CodeThales <thalles.agency@gmail.com>
2021-09-23 10:23:31 -0300

    <Create>[Controllers]: TyreController, CategoryController and PartnerController

---
+++ Equipe2-PneuStore/Controllers/TyreController.cs
@@ -0,0 +1,28 @@
+﻿using Equipe2_PneuStore.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+
+namespace ApiPneuStore.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class TyreController : ApiBaseController
+    {
+        ITyreService _service;
+
+        public TyreController(ITyreService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult Index() => ApiOk(_service.All());
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult Index(int? id) => ApiOk(_service.Get(id));
+
+    }
+}
