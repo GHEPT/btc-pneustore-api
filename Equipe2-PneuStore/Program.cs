f79a8b06cf215b374cd8f119ec945053042ddfbb
Eduardo Meirelles Nogueira <83734364+edumeirelles@users.noreply.github.com>
2021-09-23 14:12:03 -0300

    <feat>: [Configures SeedDatabase on Program.cs]

--- Equipe2-PneuStore/Program.cs
+++ Equipe2-PneuStore/Program.cs
@@ -1,11 +1,8 @@
+using Equipe2_PneuStore.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Equipe2_PneuStore
 {
@@ -13,7 +10,17 @@ namespace Equipe2_PneuStore
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<Context>();
+                context.Database.Migrate();
+                SeedDatabase.Initialize(services);
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
