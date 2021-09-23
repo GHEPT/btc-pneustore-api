16d665facb73d64470f7e60823852e05bd54111d
Eduardo Meirelles Nogueira <83734364+edumeirelles@users.noreply.github.com>
2021-09-23 02:59:52 -0300

    feat(Context) : implements DbContextOptions into class Context

--- Equipe2-PneuStore/Data/Context.cs
+++ Equipe2-PneuStore/Data/Context.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Equipe2_PneuStore.Data
 {
-    public class Context
+    public class Context : IdentityDbContext
     {
+        public Context(DbContextOptions<Context> options) : base (options) { }
     }
 }
