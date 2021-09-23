ea2a5b0c365934defdf39c6bbd5fde61a1518c16
Eduardo Meirelles Nogueira <83734364+edumeirelles@users.noreply.github.com>
2021-09-23 03:02:11 -0300

    feat(Startup) : configure context service on Satrtup

--- Equipe2-PneuStore/Startup.cs
+++ Equipe2-PneuStore/Startup.cs
@@ -1,7 +1,9 @@
+using Equipe2_PneuStore.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +34,10 @@ namespace Equipe2_PneuStore
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Equipe2_PneuStore", Version = "v1" });
             });
+
+            services.AddDbContext<Context>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("PneuStore"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
