871d3764670a4157369cbcde801df48086e3fe25
CodeThales <thalles.agency@gmail.com>
2021-09-24 20:23:37 -0300

    <Create>[AuthController]: <Register new user and create token>

---
+++ Equipe2-PneuStore/Controllers/AuthController.cs
@@ -0,0 +1,63 @@
+﻿using ApiPneuStore.Controllers;
+using Equipe2_PneuStore.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+
+namespace Equipe2_PneuStore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ApiBaseController
+    {
+        IAuthService _service;
+
+        public AuthController(IAuthService service)
+        {
+            _service = service;
+        }
+
+
+        /// <summary>
+        /// Creates a new user for the API
+        /// </summary>
+        /// <param name="identityUser"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("Register")]
+        [AllowAnonymous]
+        public IActionResult Register([FromBody] IdentityUser identityUser)
+        {
+            IdentityResult result = _service.Create(identityUser).Result;
+            identityUser.PasswordHash = null;
+            return result.Succeeded ?
+                ApiOk(identityUser) :
+                ApiBadRequest("Erro ao tentar criar usuário!");
+        }
+
+
+        /// <summary>
+        /// Get an access token for an authorized user.
+        /// </summary>
+        /// <param name="identityUser"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("Token")]
+        [AllowAnonymous]
+        public IActionResult Token([FromBody] IdentityUser identityUser)
+        {
+            try
+            {
+                return ApiOk(_service.GenerateToken(identityUser));
+            }
+            catch (Exception e)
+            {
+                return ApiBadRequest(e, e.Message);
+            }
+        }
+
+    }
+}
+
