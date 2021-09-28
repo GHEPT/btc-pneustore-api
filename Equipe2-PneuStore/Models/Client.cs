97d1c265a9b9ccdb9f7d89d01744e89c493d5378
Eduardo Meirelles Nogueira <83734364+edumeirelles@users.noreply.github.com>
2021-09-27 21:32:00 -0300

    <feat>: [Create classes Client and Address]

---
+++ Equipe2-PneuStore/Models/Client.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Equipe2_PneuStore.Models
+{
+    public class Client : IdentityUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CPF { get; set; }
+        public string Phone { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string AddressId { get; set; }
+        public List<Address> Address { get; set; }
+
+    }
+}
